96ad56843883a812261b41fdddf99498c0edc430
nomanshakib <nomanshakib.official@gmail.com>
2022-09-04 04:00:27 +0600

    js

--- js/app.js
+++ js/app.js
@@ -10,21 +10,3 @@ const allCategoriesName = async () => {
         console.log(error);
     }
 };
-
-showAllCategoriesName = (categories) => {
-    console.log(categories);
-    const newsCategoriesContainer = document.getElementById('news-categories');
-    categories.forEach((category) => {
-        const li = document.createElement('li');
-        li.classList.add('active-color')
-        li.innerHTML = `
-
-    <a onclick="loadAllNews('${category.category_id ? category.category_id : 'Not found'
-            }')" class=" text-decoration-none text-dark d-inline-block  px-3  active border-0 " href="#">${category.category_name ? category.category_name : 'Not found'
-            }</a>
-    `;
-
-        newsCategoriesContainer.appendChild(li);
-    });
-};
-allCategoriesName();
