f99117c70193d35fb243618af229a118d65d6e6f
nomanshakib <nomanshakib.official@gmail.com>
2022-09-04 03:49:49 +0600

    first-js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// Categories set, Display and others
+// Category create ,Display and other
 const allCategoriesName = async () => {
     const url = `https://openapi.programming-hero.com/api/news/categories`;
     try {
@@ -10,3 +10,21 @@ const allCategoriesName = async () => {
         console.log(error);
     }
 };
+
+showAllCategoriesName = (categories) => {
+    console.log(categories);
+    const newsCategoriesContainer = document.getElementById('news-categories');
+    categories.forEach((category) => {
+        const li = document.createElement('li');
+        li.classList.add('active-color')
+        li.innerHTML = `
+
+    <a onclick="loadAllNews('${category.category_id ? category.category_id : 'Not found'
+            }')" class=" text-decoration-none text-dark d-inline-block  px-3  active border-0 " href="#">${category.category_name ? category.category_name : 'Not found'
+            }</a>
+    `;
+
+        newsCategoriesContainer.appendChild(li);
+    });
+};
+allCategoriesName();
